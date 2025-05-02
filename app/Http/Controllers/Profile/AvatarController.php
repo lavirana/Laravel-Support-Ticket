5a6b38ad51984452d68ed976cc90c6763824ff48
Ashish Rana <ashishrana2882gmail.com>
2025-05-02 23:52:00 +0530

    added user upload avatar code

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request){
+        $path = $request->file('avatar')->store('avatars', 'public');
+        auth()->user()->update(['avatar' => $path]);
+        return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
+       //return back()->with('message', 'Avatar is changed');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Profile\AvatarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,8 +40,10 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__.'/auth.php';
