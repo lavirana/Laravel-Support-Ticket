06716d1f48e1434afdfdce98e5bc7d486074cc8e
Ashish Rana <ashishrana2882gmail.com>
2025-05-03 07:11:11 +0530

    updated avatar controller code

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request){
         $path = $request->file('avatar')->store('avatars', 'public');
+
+        if($oldavatar =  $request->user()->avatar){
+            Storage::disk('public')->delete($oldavatar);
+        }
+
+
         auth()->user()->update(['avatar' => $path]);
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
        //return back()->with('message', 'Avatar is changed');
